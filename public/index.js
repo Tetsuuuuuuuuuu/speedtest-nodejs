c4c5205eae010f3b2a6937a174a2daf1cf26a33c
Tetsuuuuuuuuuu <florian.busche2@gmail.com>
2024-01-31 23:19:18 +0100

    Test Fix

--- public/index.js
+++ public/index.js
@@ -15,9 +15,9 @@ function measureSpeed(url, size, durationSeconds, type) {
             const currentTime = performance.now();
             const elapsedTime = (currentTime - startTime) / 1000; // Convert to seconds
 
-            console.log(`${type} speed: ${bytesTransferred / elapsedTime / (1024 * 1024)} Mbps`);
+            //console.log(`${type} speed: ${bytesTransferred / elapsedTime / (1024 * 1024)} Mbps`);
 
-            if (elapsedTime >= durationSeconds) {
+            /*if (elapsedTime >= durationSeconds) {
                 xhr.abort(); // Stop the request if duration exceeded
                 const speed = (bytesTransferred / elapsedTime / (1024 * 1024)).toFixed(2); // Calculate speed in Mbps
                 resolve(speed);
@@ -25,7 +25,11 @@ function measureSpeed(url, size, durationSeconds, type) {
                 const speed = (bytesTransferred / elapsedTime / (1024 * 1024)).toFixed(2); // Calculate speed in Mbps
                 console.log(`${type} speed!: ${speed} Mbps`);
                 document.getElementById(type).innerText = `${speed} Mbps`;
-            }
+            }*/
+
+            const speed = (bytesTransferred / elapsedTime / (1024 * 1024)).toFixed(2); // Calculate speed in Mbps
+            console.log(`${type} speed!: ${speed} Mbps`);
+            document.getElementById(type).innerText = `${speed} Mbps`;
         };
 
         xhr.onload = function() {
