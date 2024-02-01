02f0daf25a4317be01260499077a0e7ce8a6a8ae
Tetsuuuuuuuuuu <florian.busche2@gmail.com>
2024-02-01 17:06:32 +0100

    Change Timeout

--- public/index.js
+++ public/index.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
             await new Promise((resolve) => {
                 setTimeout(() => {
                     resolve();
-                }, 3000);
+                }, 2000);
             });
         }
 
