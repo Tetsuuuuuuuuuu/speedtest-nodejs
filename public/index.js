17dc6bf7d3c3116a63451608f3269ce55d66d6ab
Tetsuuuuuuuuuu <florian.busche2@gmail.com>
2024-02-01 16:45:23 +0100

    Test other version

--- public/index.js
+++ public/index.js
@@ -2,21 +2,30 @@
 var downloadElement;
 var uploadElement;
 
+var amountDownloadTests = 10;
+var amountUploadTests = 10;
+
+var totalDownloadTime = 0;
+var totalUploadTime = 0;
+
 document.addEventListener("DOMContentLoaded", (event) => {
     downloadElement = document.getElementById('download');
     uploadElement = document.getElementById('upload');
 
     document.getElementById('startMeasure').addEventListener('click', () => {
-        // Use Path /download which sends 10MB of random data to test download speed
-        let downloadStartTime = new Date();
+        // Use Path /download which sends 10MB of random data to download the data and measure the time and then divide totalDownloadTime by amountDownloadTests to get the average time
 
-        fetch('/download')
-            .then(response => {
-                let downloadEndTime = new Date();
-                let downloadTime = downloadEndTime - downloadStartTime;
-                let downloadSpeed = 10 / (downloadTime / 1000);
-
-                downloadElement.innerHTML = `${downloadSpeed.toFixed(2)} MB/s`;
+        fetch('/download').then((response) => {
+            let start = performance.now();
+            response.arrayBuffer().then((buffer) => {
+                let end = performance.now();
+                totalDownloadTime += end - start;
+                amountDownloadTests--;
+                if (amountDownloadTests === 0) {
+                    downloadElement.innerText = (totalDownloadTime / 10).toFixed(2) + 'ms';
+                }
             });
+        });
+
     });
 });
